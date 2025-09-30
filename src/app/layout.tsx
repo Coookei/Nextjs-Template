15f80d9de72be9ea1bf67413c1702678cb7eb3dd
Coookei <89310401+Coookei@users.noreply.github.com>
2025-09-30 13:33:49 +0100

    chore: initialise Next.js project structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "EventSphere",
+  description:
+    "EventSphere is a dynamic and intuitive web application designed to help users discover, explore, and engage with local events happening in their city or desired location.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
