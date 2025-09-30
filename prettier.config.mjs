f771023d7347c6e25dc83ccd46f54b5c9ff8f47b
Coookei <89310401+Coookei@users.noreply.github.com>
2025-09-30 14:05:54 +0100

    chore: setup Prettier with ESLint

---
+++ prettier.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import("prettier").Config} */
+const config = {
+  arrowParens: "always",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  printWidth: 80,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  endOfLine: "lf",
+  jsxSingleQuote: false,
+  proseWrap: "preserve",
+  trailingComma: "all",
+};
+
+export default config;
