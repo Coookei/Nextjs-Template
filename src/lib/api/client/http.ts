a4fd0d6321054fd36afb7fdcd9068f28625fcf19
Coookei <89310401+Coookei@users.noreply.github.com>
2025-10-18 13:55:29 +0100

    chore: update environment variables for API base URL to use a relative URL

--- src/lib/api/client/http.ts
+++ src/lib/api/client/http.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:3000/api";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "/api";
 
 const httpClient = axios.create({
   baseURL: API_BASE_URL,
